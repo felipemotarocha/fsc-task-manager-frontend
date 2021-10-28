877bd7529a4a85d1f4cc1bec1f3d748a311870e8
Felipe Rocha <felipemota.rocha@gmail.com>
2021-10-28 07:41:27 -0300

    refactor: convert component to Class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import { useState } from "react";
+import React from "react";
 import TaskItem from "./components/TaskItem";
 
-const App = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: "1",
-            description: "Estudar Programação",
-            isCompleted: false,
-        },
-        {
-            id: "2",
-            description: "Ler",
-            isCompleted: true,
-        },
-    ]);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.state = {
+            tasks: [
+                {
+                    id: "1",
+                    description: "Estudar Programação",
+                    isCompleted: false,
+                },
+                {
+                    id: "2",
+                    description: "Ler",
+                    isCompleted: true,
+                },
+            ],
+        };
+    }
 
-    return (
-        <>
-            {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
-            ))}
-        </>
-    );
-};
+    handleStateChange() {
+        this.setState({
+            tasks: [],
+        });
+    }
+
+    render() {
+        return (
+            <>
+                {this.state.tasks.map((task) => (
+                    <TaskItem key={task.id} task={task} />
+                ))}
+                <button onClick={this.handleStateChange}>Limpar tarefas</button>
+            </>
+        );
+    }
+}
 
 export default App;
+
+// import { useState } from "react";
+// import TaskItem from "./components/TaskItem";
+
+// const App = () => {
+// const [tasks, setTasks] = useState([
+//     {
+//         id: "1",
+//         description: "Estudar Programação",
+//         isCompleted: false,
+//     },
+//     {
+//         id: "2",
+//         description: "Ler",
+//         isCompleted: true,
+//     },
+// ]);
+
+//     return (
+//         <>
+//             {tasks.map((task) => (
+//                 <TaskItem key={task.id} task={task} />
+//             ))}
+//         </>
+//     );
+// };
+
+// export default App;
