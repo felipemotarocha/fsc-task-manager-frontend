82f96d01d33a37d4af722888ea07f1dbc71e7884
Felipe Rocha <felipemota.rocha@gmail.com>
2021-11-04 08:51:26 -0300

    feat: add AddTask component

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import CustomInput from "./CustomInput";
+
+import "./AddTask.scss";
+
+const AddTask = () => {
+    const [task, setTask] = useState("");
+
+    const onChange = (e) => {
+        setTask(e.target.value);
+    };
+
+    return (
+        <div className="add-task-container">
+            <CustomInput
+                label="Adicionar tarefa..."
+                value={task}
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+export default AddTask;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./Tasks.scss";
 
 import TaskItem from "./TaskItem";
+import AddTask from "./AddTask";
 
 const Tasks = () => {
     const [tasks, setTasks] = useState([]);
@@ -28,6 +29,7 @@ const Tasks = () => {
 
             <div className="last-tasks">
                 <h3>Últimas Tarefas</h3>
+                <AddTask />
                 <div className="tasks-list">
                     {tasks
                         .filter((task) => task.isCompleted === false)
