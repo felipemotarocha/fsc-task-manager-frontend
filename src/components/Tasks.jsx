b576c3ec6597f6c6513810b6f20f2367de8a16fa
Felipe Rocha <felipemota.rocha@gmail.com>
2021-11-09 08:26:37 -0300

    feat: add key to Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -37,7 +37,11 @@ const Tasks = () => {
                     {tasks
                         .filter((task) => task.isCompleted === false)
                         .map((lastTask) => (
-                            <TaskItem task={lastTask} fetchTasks={fetchTasks} />
+                            <TaskItem
+                                key={lastTask._id}
+                                task={lastTask}
+                                fetchTasks={fetchTasks}
+                            />
                         ))}
                 </div>
             </div>
@@ -49,6 +53,7 @@ const Tasks = () => {
                         .filter((task) => task.isCompleted)
                         .map((completedTask) => (
                             <TaskItem
+                                key={completedTask._id}
                                 task={completedTask}
                                 fetchTasks={fetchTasks}
                             />
