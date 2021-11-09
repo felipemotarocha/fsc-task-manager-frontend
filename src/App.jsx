9cfe83cb137f3697f6088656b930894443994fa3
Felipe Rocha <felipemota.rocha@gmail.com>
2021-11-09 08:03:13 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Sidebar from "./components/Sidebar";
 import Tasks from "./components/Tasks";
+
+import "./App.scss";
 
 const App = () => {
     return (
-        <>
+        <div className="app-container">
+            <Sidebar />
             <Tasks />
-        </>
+        </div>
     );
 };
 
