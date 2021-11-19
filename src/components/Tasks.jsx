882536cd99757d6c85689bb3566f0262114e5ad3
Felipe Rocha <felipemota.rocha@gmail.com>
2021-11-19 08:40:35 -0300

    perf: add fetchTasks to useCallback

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import axios from "axios";
 import { useAlert } from "react-alert";
 
@@ -12,7 +12,7 @@ const Tasks = () => {
 
     const alert = useAlert();
 
-    const fetchTasks = async () => {
+    const fetchTasks = useCallback(async () => {
         try {
             const { data } = await axios.get("http://localhost:8000/tasks");
 
@@ -20,7 +20,7 @@ const Tasks = () => {
         } catch (_error) {
             alert.error("Não foi possível recuperar as tarefas.");
         }
-    };
+    }, [alert]);
 
     const lastTasks = useMemo(() => {
         return tasks.filter((task) => task.isCompleted === false);
@@ -32,7 +32,7 @@ const Tasks = () => {
 
     useEffect(() => {
         fetchTasks();
-    }, []);
+    }, [fetchTasks]);
 
     return (
         <div className="tasks-container">
