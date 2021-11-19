0d2992901d9fd8509b42fb1cc7b1ceb275774ef3
Felipe Rocha <felipemota.rocha@gmail.com>
2021-11-19 07:59:52 -0300

    feat: add Login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import CustomButton from "../components/CustomButton";
+
+import "./Logo.scss";
+import logo from "../assets/images/logo-2.png";
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="Full Stack Club" />
+            <div className="button-container">
+                <CustomButton>Entrar</CustomButton>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
